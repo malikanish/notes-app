a5cfeeb5da192090ca233c4d00d7520251b33ac6
malikanish <anishbilal001@gmail.com>
2025-08-13 11:26:26 +0500

    chnges

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+APP_VERSION="${APP_VERSION:-latest}"  # Default latest, but can be overridden
+
 # Update packages
 sudo yum update -y
 
@@ -16,6 +18,6 @@ sudo ./aws/install
 # Login to AWS ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 014498660251.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull and run cont
-docker pull 014498660251.dkr.ecr.us-east-1.amazonaws.com/myapp-repo:latest
-docker run -d -p 8000:8000 014498660251.dkr.ecr.us-east-1.amazonaws.com/myapp-repo:latest
+# Pull and run specific version
+docker pull 014498660251.dkr.ecr.us-east-1.amazonaws.com/myapp-repo:$APP_VERSION
+docker run -d -p 8000:8000 014498660251.dkr.ecr.us-east-1.amazonaws.com/myapp-repo:$APP_VERSION
